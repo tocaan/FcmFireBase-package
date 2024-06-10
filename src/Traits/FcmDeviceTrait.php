b70f4d8e58304dd87e96e01bde2802a6510d4f62
AhmedSafwat1 <Ahmed.safwat1235@gmail.com>
2024-06-10 19:23:32 +0300

    Fix  issue

--- src/Traits/FcmDeviceTrait.php
+++ src/Traits/FcmDeviceTrait.php
@@ -7,9 +7,9 @@ trait FcmDeviceTrait
     public function deviceTokens()
     {
         if(config("fcm-firebase.allow_morph", false)) {
-            return $this->morphMany(config("fcm-firebase.model"), config("fcm-firebase.morph"));
+            return $this->morphMany(config("fcm-firebase.device_model"), config("fcm-firebase.morph"));
         } else {
-            return $this->hasMany(config("fcm-firebase.model"), config("fcm-firebase.user_column"));
+            return $this->hasMany(config("fcm-firebase.device_model"), config("fcm-firebase.user_column"));
         }
     }
 }
